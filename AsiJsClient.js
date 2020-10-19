8f9be72001bfecb12f1aeb6b74845524550a2125
Kimberley Elcess <kelcess@advsol.com>
2020-10-19 07:08:52 -0500

    update partyId
    - update partyId in main program
    - update dependency name

--- AsiJsClient.js
+++ AsiJsClient.js
@@ -1,9 +1,9 @@
 var server = require("./LocalServer.js");
 var authorize = require("./GetToken.js");
-var party = require("./GetParty101");
+var party = require("./GetPartyById");
 var secrets = require("./secrets.js");
 const { baseUri } = require("./secrets.js");
-var partyId = "10001";
+var partyId = "55fab524-9e8f-4c81-8057-01ae34eb447a";
 
 async function sleep() {
     await setTimeout(() => {}, 20000);
