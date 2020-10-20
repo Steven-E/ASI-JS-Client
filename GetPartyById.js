387640b53876abf6492f5d1990e8a8e336482af9
Kimberley Elcess <kelcess@advsol.com>
2020-10-20 09:38:36 -0500

    remove security bypass
    remove environment variable setting bypassing security

--- GetPartyById.js
+++ GetPartyById.js
@@ -14,8 +14,6 @@ module.exports = async function GetParty(accessToken, id, baseUri) {
     var endpoint = "api/Party/" + id;
     var fetchUrl = baseUri + endpoint;
 
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
     try {
         var response = await fetch(fetchUrl, requestOptions);
         var result = await response.json();
